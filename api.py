24b24583515cf2bdfc4509d545b7bab82aeba4a1
Felix Knoepke <morytox@hotmail.de>
2019-05-10 16:02:48 +0200

    switched to flask-mqtt

--- api.py
+++ api.py
@@ -2,35 +2,41 @@ import os
 import uuid
 import json
 from distutils.util import strtobool
-import paho.mqtt.client as mqtt
 import flask
 from flask import request, json
+from flask_mqtt import Mqtt
 
 
 
-websocket = strtobool(os.getenv('MQTT_SOCKET', 'False'))
+app = flask.Flask(__name__)
+app.config['SECRET'] = 'my secret key'
+app.config['MQTT_BROKER_URL'] = 'ts.rdy.one'
+app.config['MQTT_BROKER_PORT'] = 11883
+app.config['MQTT_USERNAME'] = ''
+app.config['MQTT_PASSWORD'] = ''
+app.config['MQTT_KEEPALIVE'] = 60
+app.config['MQTT_TLS_ENABLED'] = False
+
 pub_name = os.getenv('HOSTNAME', ('publisher-' + uuid.uuid4().hex.upper()[0:6]))
-if websocket:
-    client = mqtt.Client(pub_name,transport='websockets')
-else:
-    client = mqtt.Client(pub_name)
 
-def create_app():
-    app = flask.Flask(__name__)
-    def run_on_start(*args, **argv):
-        broker = os.getenv('MQTT_BROKER', 'ts.rdy.one')
-        port =  int(os.getenv('MQTT_PORT', 11883))
-        wait_timer = int(os.getenv('MQTT_WAITTIME', 1))
+mqtt = Mqtt(app)
 
-        print ("Run init sequence")
-        print ("Connecting ...")
-        client.connect(broker, port, 60)
-        print ("Starting Loop...")
-        client.loop_start()
-    run_on_start()
-    return app
-app = create_app()
-app.config["DEBUG"] = True
+#def create_app():
+#    app = flask.Flask(__name__)
+#    def run_on_start(*args, **argv):
+#        broker = os.getenv('MQTT_BROKER', 'ts.rdy.one')
+#        port =  int(os.getenv('MQTT_PORT', 11883))
+#        wait_timer = int(os.getenv('MQTT_WAITTIME', 1))
+#
+#        print ("Run init sequence")
+#        print ("Connecting ...")
+#        client.connect(broker, port, 60)
+#        print ("Starting Loop...")
+#        client.loop_start()
+#    run_on_start()
+#    return app
+#app = create_app()
+#app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
@@ -43,11 +49,10 @@ def api_message():
         message = request.json
         #json.dumps(request.json)
         #message = {'speed': 0, 'steering': 0}
-          
         speed_perc = message["speed"]
-        client.publish('car/speed', int(speed_perc))
+        mqtt.publish('car/speed', int(speed_perc))
         angle_perc = message["steering"]
-        client.publish('car/steering', int(angle_perc))
+        mqtt.publish('car/steering', int(angle_perc))
         return "Messages Sent!"
 
     else:
@@ -68,3 +73,5 @@ def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
 app.run()
+
+
