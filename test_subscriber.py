82df95bcf3632f80414ee12a6b08d285d457143c
Felix Knoepke <morytox@hotmail.de>
2019-05-10 16:36:44 +0200

    fixed test_subscriber

--- test_subscriber.py
+++ test_subscriber.py
@@ -4,10 +4,12 @@ import uuid
 import os
 import json
 import sys
+import time
 
 from distutils.util import strtobool
 
 import paho.mqtt.client as mqtt
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected as %s with result code %s" % (sub_name, str(rc)))
@@ -29,21 +31,27 @@ def sigterm_handler(signal, frame):
     print('System shutting down, closing connection')
     sys.exit(0)
 
-#signal.signal(signal.SIGTERM, sigterm_handler)
+signal.signal(signal.SIGTERM, sigterm_handler)
+
 
 
 def main():
 
-    # print received messages to the screen of the device
-    client = mqtt.Client("testeee")
     client.on_connect = on_connect
     client.on_message = on_message
-    print("Connecting now...")
-    client.connect("ts.rdy.one", 11883, 60)
+    client.connect(broker, port, 60)
 
-    client.loop_forever()
+    client.loop_start()
+    time.sleep(60)
+    client.loop_stop()
+
 
 if __name__ == '__main__':
+    broker = os.getenv('MQTT_BROKER', 'ts.rdy.one')
+    port =  int(os.getenv('MQTT_PORT', 11883))
+    sub_name = os.getenv('HOSTNAME', ('subscriber-' + uuid.uuid4().hex.upper()[0:6]))
+    websocket = strtobool(os.getenv('MQTT_SOCKET', 'False'))
+    client = mqtt.Client(sub_name)
     main()
 
 
